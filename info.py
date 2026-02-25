ab7fe92b248b40cb5f1381b6977b0e0fad16c5a7
AKMOVIES72 <akmovies728@gmail.com>
2026-02-25 09:24:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -170,10 +170,10 @@ MULTI_CLIENT = False
 SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if "DYNO" in environ:
-    ON_HEROKU = True
+    ON_HEROKU = False 
 else:
-    ON_HEROKU = True
-URL = environ.get("FQDN", "https://ak-filter-botss-1.onrender.com")
+    ON_HEROKU = False 
+URL = environ.get("FQDN", "")
 
 # Commands
 admin_cmds = [
